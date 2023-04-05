b3e70ca8af32fb8e10034372fa9f0432080795d9
Tomoya Anzai <46628686+nyon3@users.noreply.github.com>
2023-04-05 06:04:18 +0000

    fix the issue of the contents inside the jumbotron

--- components/UI/Banner/index.js
+++ components/UI/Banner/index.js
@@ -4,7 +4,7 @@ const Banner = () => {
   return (
     <div className={styles.banner}>
       <p className={styles.message}>
-        2023.04.01 英会話カフェはコロナの影響でやっていません
+        英会話カフェは現在、新型コロナウイルスの影響により営業を休止しております。
       </p>
     </div>
   );
