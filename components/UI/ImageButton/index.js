1145c43e8384954678da4222eeca212ec7ed646e
Tomoya Anzai <46628686+nyon3@users.noreply.github.com>
2023-04-05 07:32:10 +0000

    relink trail component

--- components/UI/ImageButton/index.js
+++ components/UI/ImageButton/index.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import testPic from "../../../public/images/study-textbook.jpg";
 
 const Button = () => (
-  <Link href="/">
+  <Link href="https://forms.gle/wRLWN8WiLdQLZdLQ9">
     <div className={styles.button}>
       <Image src={testPic} alt="test" width={500} height={500} />
       <p className={styles.text}>体験レッスンに申し込む</p>
