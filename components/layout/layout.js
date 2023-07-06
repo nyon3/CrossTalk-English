cb0c55a6a1e4c5a3847a845ebf5067a6a5be3a4c
nyon3 <tomoya@crosstalk.me>
2023-07-06 17:21:31 +0900

    Changed site name

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -4,7 +4,19 @@ import styles from "./styles.module.css";
 import Footer from "../UI/Footer";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
-const Layout = ({ children, title = "岡山市の英会話スクール | CrossTalkで楽しく英語を学ぶ", description = "岡山市にあるCrossTalk英会話スクールでは、楽しく英語を話すことをモットーにしています。経験豊富な講師が担当し、社会人向けの最適な内容で英会話を学べます。仕事でも活用できる英語コミュニケーション能力を身につけましょう。" }) => {
+const Layout = ({
+  children,
+  title = "岡山市の英会話スクール | CrossTalkで楽しく英語を学ぶ",
+  description = "岡山市にあるCrossTalk英会話スクールでは、楽しく英語を話すことをモットーにしています。",
+}) => {
+  let structuredData = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    url: "https://crosstalk.me/",
+    name: "CrossTalk英会話スクール",
+    // alternateName: "An Alternate Name", // 代替名があればここに書く
+  };
+
   return (
     <>
       <GoogleAnalytics trackPageViews />
@@ -14,6 +26,11 @@ const Layout = ({ children, title = "岡山市の英会話スクール | CrossTa
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          <script
+            key="jsonld"
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
+          />
         </Head>
         <Navbar />
         {children}
