7ca455be0bdb81d9cbcf1b67862e7766143cd991
nyon3 <tomoya@crosstalk.me>
2023-07-12 15:11:54 +0900

    update design

--- components/UI/PriceComponent/index.js
+++ components/UI/PriceComponent/index.js
@@ -20,79 +20,53 @@ const PricingCard = ({ courseType }) => {
 
   // Define the content of the card based on the course type
   // Here you can further customize the pricing details for each type of course
-  if (courseType === "group_lesson") {
+  if (courseType === "group_lesson" || courseType === "free_talk") {
     // Pricing for group lessons
     Price = (
-      <>
-        <div className={styles.price}>
-          <span>月額料金:</span>
-          <span>¥13,200</span>
-        </div>
-        <div className={styles.fee}>
-          <span>入会費:</span>
-          <span>¥5,000</span>
-        </div>
-        <div className={styles.fee}>
-          <span>テキスト代:</span>
-          <span>¥3,500</span>
-        </div>
-        <div className={styles.lesson}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>レッスン料金</h2>
+        <div className={styles.courseDescription}>
           <span>50分のレッスン x 4回</span>
         </div>
-      </>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>月額料金</span>
+          <span className={styles.monthly_fee}>¥13,200</span>
+        </div>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>入会費</span>
+          <span className={styles.monthly_fee}>¥5,000</span>
+        </div>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>テキスト</span>
+          <span className={styles.monthly_fee}>¥3,500</span>
+        </div>
+      </div>
     );
   } else if (courseType === "private_lesson") {
     // Pricing for private lessons
     Price = (
-      <>
-        <div className={styles.price}>
-          <span>月２回:</span>
-          <span>¥11,000</span>
-          <span>月４回:</span>
-          <span>¥22,000</span>
-        </div>
-        <div className={styles.fee}>
-          <span>入会費:</span>
-          <span>¥5,000</span>
-        </div>
-        <div className={styles.fee}>
-          <span>テキスト代:</span>
-          <span>¥3,500</span>
-        </div>
-        <div className={styles.lesson}>
+      <div className={styles.card}>
+        <h2 className={styles.title}>レッスン料金</h2>
+        <div className={styles.courseDescription}>
           <span>50分のレッスン x 4回</span>
         </div>
-      </>
-    );
-  } else if (courseType === "free_talk") {
-    // Pricing for free talk sessions
-    Price = (
-      <>
-        <div className={styles.price}>
-          <span>月額料金:</span>
-          <span>¥13,200</span>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>月額料金</span>
+          <span className={styles.monthly_fee}>¥22,000</span>
         </div>
-        <div className={styles.fee}>
-          <span>入会費:</span>
-          <span>¥5,000</span>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>入会費</span>
+          <span className={styles.monthly_fee}>¥5,000</span>
         </div>
-        <div className={styles.fee}>
-          <span>テキスト代:</span>
-          <span>¥3,500</span>
+        <div className={styles.fee_label}>
+          <span className={styles.fee_title}>テキスト</span>
+          <span className={styles.monthly_fee}>¥3,500</span>
         </div>
-        <div className={styles.lesson}>
-          <span>50分のレッスン x 4回</span>
-        </div>
-      </>
+      </div>
     );
   }
 
-  return (
-    <div className={styles.card}>
-      <h2 className={styles.title}>レッスン料金</h2>
-      {Price}
-    </div>
-  );
+  return <>{Price}</>;
 };
 
 export default PricingCard;
