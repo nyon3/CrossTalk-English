a6a17eca69cfc74f195a8b5c0d3a358bb0f4ea8d
nyon3 <tomoya@crosstalk.me>
2023-09-27 13:44:51 +0900

    add canonical

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 // Components
 import Layout from "@/components/Layout/layout";
+import Head from "next/head";
 
 export default function TemporaryLanding() {
   return (
-    <Layout 
-      title="Welcome to CrossTalk" 
+    <Layout
+      title="Welcome to CrossTalk"
       description="Navigate to the service you're interested in."
     >
+      <Head>
+        <link rel="canonical" href="https://crosstalk.me/eikaiwa-school" />
+      </Head>
       <p>Redirecting...</p>
     </Layout>
   );
