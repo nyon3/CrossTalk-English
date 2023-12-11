6b9877bea3b88072b7e8a82acef3e11962fe4ba4
nyon3 <tomoya@crosstalk.me>
2023-12-11 20:10:17 +0900

    Remove next-sitemap configuration and update next.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://crosstalk.me/',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+  // Other configurations...
+  transform: async (config, path) => {
+      // You can customize how URLs are added to your sitemap here.
+      // For example, you can set a higher priority for your new homepage.
+      return {
+          loc: path, // The URL of the page
+          changefreq: 'monthly', // Frequency of page update
+          priority: path === '/eikaiwa-school' ? 1.0 : 0.7, // Homepage gets priority 1, others get 0.7
+          lastmod: config.autoLastmod ? new Date().toISOString() : undefined, // Use current date as the last modification date
+      }
+  },
+};
