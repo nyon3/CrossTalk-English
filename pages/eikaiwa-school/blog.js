693e26585ea4133a32e9818ae5e361105041b79d
nyon3 <tomoya@crosstalk.me>
2023-12-15 21:57:24 +0900

    Delete news.js file

--- pages/eikaiwa-school/blog.js
+++ pages/eikaiwa-school/blog.js
@@ -3,24 +3,24 @@ import { getSortedPostsData } from "../../lib/news";
 import Layout from "@/components/Layout/layout";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const postsData = getSortedPostsData("blog");
   return {
     props: {
-      allPostsData,
+      postsData,
     },
   };
 }
 
-export default function Blog({ allPostsData }) {
+export default function Blog({ postsData }) {
   return (
     <Layout>
       <main>
         <div>
-          <h1>お知らせ</h1>
+          <h1>ブログ</h1>
           <ul>
-            {allPostsData.map(({ slug, date, title }) => (
+            {postsData.map(({ slug, date, title }) => (
               <li key={slug}>
-                <Link href={`/news/${slug}`}>{title}</Link>
+                <Link href={`/blog/${slug}`}>{title}</Link>
                 <br />
                 <small>{new Date(date).toLocaleDateString()}</small>
               </li>
